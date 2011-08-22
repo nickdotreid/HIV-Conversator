372a34be6fb6b2f5eee4a82ebf7be9b3f8c3c3d1
Nick Reid <nickreid@nickreid.com>
2011-08-22 09:19:34 -0700

    made query twitter script better

--- scrape_twitter.py
+++ scrape_twitter.py
@@ -4,6 +4,9 @@ from twython import Twython
 twitter = Twython()
 
 def query_twitter(query_terms):
+	term = add_term(query_terms)
 	query = twitter.searchTwitter(geocode="37.62,-122.38,15mi",q=query_terms)
 	for tweet in query['results']:
-		add_tweet(tweet)
+		tweet = add_tweet(tweet)
+		term.tweets.append(tweet)
+	db.session.commit()
