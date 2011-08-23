2f3148b4d3399393fbb1295325fba34e1360a455
Nick Reid <nickreid@nickreid.com>
2011-08-22 18:22:19 -0700

    added basic pattern

--- static/page.js
+++ static/page.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-	
+	$("#graph").bind('fetch',function(event){
+		date = event.date
+		$.ajax({
+			url:"/tweets",
+			type:'post',
+			dataType:'json',
+			data:{
+				date:date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getYear()
+			},
+			success:function(data){
+				if(data['tweets']){
+					// add new bar
+					$("#graph").trigger({
+						type:"fetch",
+						date:new Date(event.date.getTime()-86400000)
+					})
+				}
+			}
+		})
+	}).trigger({
+		type:'fetch',
+		date:new Date()
+	})
 })
