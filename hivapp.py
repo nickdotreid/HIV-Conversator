d1cf0515783276803314f1059813152cafd34057
Nick Reid <nickreid@nickreid.com>
2011-08-22 18:03:31 -0700

    add basic templates

--- hivapp.py
+++ hivapp.py
@@ -11,3 +11,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db = SQLAlchemy(app)
 
 from models import *
+
+@app.route("/")
+def index():
+	return render_template('index.html')
+	
+@app.route('/tweets',methods=['GET','POST'])
+def get_tweets():
+	total = 0
+	if request.method == "POST" and 'region' in request.form and 'types' in request.form:
+		total = 10
+	return jsonify(total = total)
--- scrape_twitter.py
+++ scrape_twitter.py
@@ -3,9 +3,9 @@ from twython import Twython
 
 twitter = Twython()
 
-def query_twitter(query_terms):
+def query_twitter(query_terms, page=1):
 	term = add_term(query_terms)
-	query = twitter.searchTwitter(geocode="37.62,-122.38,15mi",q=query_terms)
+	query = twitter.searchTwitter(geocode="37.62,-122.38,15mi",q=query_terms,rpp='100',page=str(page))
 	for tweet in query['results']:
 		tweet = add_tweet(tweet)
 		term.tweets.append(tweet)
